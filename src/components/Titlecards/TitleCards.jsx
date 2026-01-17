6a36a80ba7e6566a88c3c11ff6f45bda28f25489
Naveen Kumar Ray <218852547+NaveenRay1@users.noreply.github.com>
2026-01-17 14:10:47 +0530

    added mid section

--- src/components/Titlecards/TitleCards.jsx
+++ src/components/Titlecards/TitleCards.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import './TitleCards'
+
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import React, { useEffect, useRef } from 'react'
 const TitleCards = () => {
+    const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, [])
   return (
-    <div className='title'>
-      
+    <div className='titlecards'>
+        <h2>Popular on Netflix</h2>
+        <div className="card-list" ref={cardsRef}>
+            {cards_data.map((card, index)=>{
+                return <div className="card" key={index}>
+                    <img src={card.image} alt="" />
+                    <p>{card.name}</p>
+                </div>
+            })}
+        </div>
     </div>
   )
 }
